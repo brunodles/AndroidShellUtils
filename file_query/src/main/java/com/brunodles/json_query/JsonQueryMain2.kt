cb1c0dd5dfeb258fc552811be5773d7fbc38c67c
Bruno de Lima e Silva <dlimaun@gmail.com>
2023-06-04 10:25:42 -0300

    Use the new functions on Scratch

--- file_query/src/main/java/com/brunodles/json_query/JsonQueryMain2.kt
+++ file_query/src/main/java/com/brunodles/json_query/JsonQueryMain2.kt
@@ -1,6 +1,7 @@
 package com.brunodles.json_query
 
 import com.brunodles.json_query.ExtraFunctions.present
+import com.brunodles.json_query.ExtraFunctions.println
 import com.brunodles.tablebuilder.ColumnDirection.right
 import java.io.File
 
@@ -12,34 +13,32 @@ object JsonQueryMain2 {
             rootDir = File("/home/bruno/workspace/android/endeavor/wallpaper/database/filesv4/_output/boards"),
         )
 
-        println(
-            database.newQuery {
-                select(
-                    { field["key"] },
-                    { field["name"] },
-                    { field["imagesCount"] },
-                    { field["pinCount"] },
-                    { field["pins"].count() },
-                    { field["enabled"] },
-                    { field["lastChange"] },
-                    { file }
-                )
-                from { file.absolutePath.contains("boards") }
-                where {
+        database.newQuery {
+            select(
+                { field["key"] },
+                { field["name"] },
+                { field["imagesCount"] },
+                { field["pinCount"] },
+                { field["pins"].count() },
+                { field["enabled"] },
+                { field["lastChange"] },
+                { file }
+            )
+            from { file.absolutePath.contains("boards") }
+            where {
 //                    it["enabled"] eq true
-                    field["name"] contains "card"
-                }
-            }.present {
-                add("key")
-                add("name")
-                add("imagesCount", right)
-                add("pinCount", right)
-                add("realPinCount", right)
-                add("enabled")
-                add("lastChange")
-                add("file")
+                field["name"] contains "card"
             }
-        )
+        }.present {
+            add("key")
+            add("name")
+            add("imagesCount", right)
+            add("pinCount", right)
+            add("realPinCount", right)
+            add("enabled")
+            add("lastChange")
+            add("file")
+        }.println()
     }
 }
 
