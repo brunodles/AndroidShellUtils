691d8391058027228c56579b1622f45b241f0a91
Bruno de Lima e Silva <dlimaun@gmail.com>
2023-06-06 01:41:40 -0300

    Update Element to accept null values

--- file_query/src/main/java/com/brunodles/file_query/Element.kt
+++ file_query/src/main/java/com/brunodles/file_query/Element.kt
@@ -4,22 +4,24 @@ import com.google.gson.JsonArray
 import com.google.gson.JsonObject
 import com.google.gson.JsonPrimitive
 
-class Element<T : Any>(
+class Element<T : Any?>(
     private val element: T
 ) {
     operator fun get(key: String): Element<*> {
         if (element is JsonObject)
             return Element(element.get(key))
-        val elementClass = element::class.java
-        try {
-            return Element(elementClass.getDeclaredField(key).get(element))
-        } catch (_: Exception) {
+        this.element?.let { element ->
+            val elementClass = element::class.java
+            try {
+                return Element(elementClass.getDeclaredField(key).get(element))
+            } catch (_: Exception) {
+            }
+            try {
+                return Element(elementClass.getDeclaredMethod(key).invoke(elementClass))
+            } catch (_: Exception) {
+            }
         }
-        try {
-            return Element(elementClass.getDeclaredMethod(key).invoke(elementClass))
-        } catch (_: Exception) {
-        }
-        throw IllegalArgumentException("Unable to get the field(\"$key\").")
+        return Element(null)
     }
 
     infix fun eq(value: Int): Boolean {
@@ -72,7 +74,7 @@ class Element<T : Any>(
             is Number -> element.toDouble()
             is String -> element.toDouble()
             is JsonPrimitive -> element.asDouble
-            else -> throw IllegalArgumentException("Unable to parse to Double. Type: \"${element::class.java.simpleName}\" Value:\"$element\".")
+            else -> throw IllegalArgumentException("Unable to parse to Double. Type: \"${elementClassName()}\" Value:\"$element\".")
         }
     }
 
@@ -81,20 +83,21 @@ class Element<T : Any>(
             is Number -> element.toLong()
             is String -> element.toLong()
             is JsonPrimitive -> element.asLong
-            else -> throw IllegalArgumentException("Unable to parse to Long. Type: \"${element::class.java.simpleName}\" Value:\"$element\".")
+            else -> throw IllegalArgumentException("Unable to parse to Long. Type: \"${elementClassName()}\" Value:\"$element\".")
         }
     }
 
-    fun count() : Element<Int> {
+    fun count(): Element<Int> {
         return when (element) {
             is Array<*> -> element.size
             is List<*> -> element.size
-            is Map<*,*> -> element.size
+            is Map<*, *> -> element.size
             is String -> element.length
             is JsonObject -> element.size()
             is JsonArray -> element.size()
-            else -> throw IllegalArgumentException("Unable to count on this type. Type: \"${element::class.java.simpleName}\" Value:\"$element\".")
+            else -> throw IllegalArgumentException("Unable to count on this type. Type: \"${elementClassName()}\" Value:\"$element\".")
         }.let { Element(it) }
     }
 
+    private fun elementClassName(): String = element?.let { it::class.java.simpleName } ?: "null"
 }
