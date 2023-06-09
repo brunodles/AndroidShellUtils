e17e3e369160f9ab26b45298f3cb505985a48c08
Bruno Lima <bruno.lima@uber.com>
2023-06-09 02:54:55 -0300

    Create yaml sample_data

--- query_interpreter/src/main/groovy/com/brunodles/groovy/GroovyScratch.groovy
+++ query_interpreter/src/main/groovy/com/brunodles/groovy/GroovyScratch.groovy
@@ -1,27 +1,41 @@
 package com.brunodles.groovy
 
 import groovy.json.JsonOutput
+import groovy.yaml.YamlBuilder
+import org.apache.groovy.yaml.util.YamlConverter
 
 import static com.brunodles.groovy.ExtensionFunctions.registerCustomExtensionFunctions
+import static com.brunodles.groovy.ExtensionFunctions.tryOrNull
 
 class GroovyScratch {
 
     static void main(String[] args) {
         registerCustomExtensionFunctions()
 
-        def jsonDir = new File("/Users/bruno.lima/workspace/AndroidShellUtils/sample_data/images/")
-        jsonDir.mkdirs()
-        def json = new File("/Users/bruno.lima/workspace/AndroidShellUtils/sample_data/images.json")
-            .toJson()
+        def jsonDir = new File("/Users/bruno.lima/workspace/AndroidShellUtils/sample_data/boards/")
+        jsonDir.listFiles()
+            .sort()
+            .take(10)
+            .each {file ->
+                tryOrNull {
+                    def outputFIle = new File(file.absolutePath.replace(".json", ".yaml"))
+                    outputFIle.write(YamlConverter.convertJsonToYaml(file.newReader()))
+                }
+            }
+//        def file = new File("/Users/bruno.lima/workspace/AndroidShellUtils/sample_data/boards/a_certain_scientific_railgun.json")
+//        def json = file.readJson()
 
-        json.forEach { content->
-            content.pins = []
-            content.url = null
-            content.originalUrl = null
-            def boardFile = new File(jsonDir, "${content.key}.json")
-            boardFile.write(JsonOutput.toJson(content))
-            println "completed ${content.key}"
-        }
+//        def builder = new YamlBuilder()
+//        outputFIle.write(builder(json).toString())
+
+//        json.forEach { content->
+//            content.pins = []
+//            content.url = null
+//            content.originalUrl = null
+//            def boardFile = new File(jsonDir, "${content.key}.json")
+//            boardFile.write(JsonOutput.toJson(content))
+//            println "completed ${content.key}"
+//        }
     }
 
 }
