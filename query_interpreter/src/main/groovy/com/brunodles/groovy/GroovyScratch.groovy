a8e3809d8705adb61bae91d8e31080d21cb54837
Bruno Lima <bruno.lima@uber.com>
2023-06-09 01:21:14 -0300

    Generate sample data

--- query_interpreter/src/main/groovy/com/brunodles/groovy/GroovyScratch.groovy
+++ query_interpreter/src/main/groovy/com/brunodles/groovy/GroovyScratch.groovy
@@ -1,44 +1,27 @@
 package com.brunodles.groovy
 
+import groovy.json.JsonOutput
+
 import static com.brunodles.groovy.ExtensionFunctions.registerCustomExtensionFunctions
-import static com.brunodles.groovy.FormatFunctions.*
-import static java.lang.Math.*
 
 class GroovyScratch {
 
     static void main(String[] args) {
         registerCustomExtensionFunctions()
-        def files = new ArrayList<File>()
-//        new File("./")
-        new File("/Users/bruno.lima/workspace/scratch/working_area/tmp_sync")
-                .eachFileRecurse { file -> if (file.isFile()) files.add(file) }
 
-//        files.collect { it.toJson() }
-//            .findAll { it.chunkUUID }
-////            .forEach { println it["chunkUUID"] }
-//            .forEach {
-//                println it
-//                try {
-//                    println "\t${it.chunkUUID}"
-//                } catch (Exception e) {
-//                    println "\tError ${e.message}"
-//                }
-//            }
-        def result = GQ {
-            from chunk in files.collect { it.toJson() }
-            where chunk.chunkUUID != null
-            select chunk.chunkUUID,
-                    chunk.chunkIndexOnSegment,
-                    round((chunk.endTimeInMillis - chunk.startTimeInMillis) / 60000) as duration,
-                    dateFormat(chunk.startTimeInMillis, "HH:MM:ss") as date
+        def jsonDir = new File("/Users/bruno.lima/workspace/AndroidShellUtils/sample_data/images/")
+        jsonDir.mkdirs()
+        def json = new File("/Users/bruno.lima/workspace/AndroidShellUtils/sample_data/images.json")
+            .toJson()
+
+        json.forEach { content->
+            content.pins = []
+            content.url = null
+            content.originalUrl = null
+            def boardFile = new File(jsonDir, "${content.key}.json")
+            boardFile.write(JsonOutput.toJson(content))
+            println "completed ${content.key}"
         }
-
-        def resultList = result.toList()
-        println resultList.collect {it.nameList.withIndex().collectEntries { value, index -> [index, value] }
-        }.inject { aggregator, value -> aggregator+value}
-            .sort()
-            .collect { index, name -> name }
-        println resultList.join("\n")
     }
 
 }
